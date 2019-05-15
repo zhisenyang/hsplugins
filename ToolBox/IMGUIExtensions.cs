42cbbd5b66e3b953a3854d7357ba7a0ef9c3079c
Joan FRUTOZO <joan.frutozo@epitech.eu>
2019-05-15 18:03:59 +0200

    oubli

---
+++ ToolBox/IMGUIExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ToolBox
+{
+    public static class IMGUIExtensions
+    {
+        private static int _cachedFontSize;
+
+        public static void SetFontSize(int size)
+        {
+            _cachedFontSize = GUI.skin.label.fontSize;
+            foreach (GUIStyle style in GUI.skin)
+            {
+                style.fontSize = size;
+            }
+        }
+
+        public static void ResetFontSize()
+        {
+            foreach (GUIStyle style in GUI.skin)
+            {
+                style.fontSize = _cachedFontSize;
+            }
+        }
+    }
+}
